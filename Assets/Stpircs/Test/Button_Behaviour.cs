1424f97c29e9b8ffa0d98d3c85e3de382b56b300
Terramen <mrdimon4eg@gmail.com>
2023-09-14 16:44:11 +0200

    New random

---
+++ Assets/Stpircs/Test/Button_Behaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Button_Behaviour : MonoBehaviour
+{
+    
+    public void OnClick(GameObject clicked_button)
+    {
+    
+        
+        GameObject MainInstance = GameObject.Find("Game");
+        
+        // sending the chosen gameObject from inspector MainGame.Image_On_CLick
+        MainInstance.GetComponent<MainGame>().Image_On_Click(clicked_button); 
+    
+    }
+
+}
