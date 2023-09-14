6b7bf7d3b3495278349a6ebec4a55927c24cfe47
Terramen <mrdimon4eg@gmail.com>
2023-09-14 16:44:11 +0200

    New random

---
+++ Assets/Stpircs/Test/Loading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField]
+    private Image loadingline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SceneShift());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loadingline.fillAmount += .05F;
+    }
+
+    IEnumerator SceneShift()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Main_Menu");
+
+    }
+
+}
