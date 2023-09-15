04f78ff375da9386aec940282e89a9502cda3529
Terramen <mrdimon4eg@gmail.com>
2023-09-15 17:29:29 +0200

    New top script

---
+++ Assets/Stpircs/TopScript.cs
@@ -0,0 +1,673 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopScript : MonoBehaviour
+{
+// Start is called before the first frame update
+    void Start()
+    {
+        RandomMethod_1();
+        RandomMethod_2();
+        RandomMethod_3();
+        RandomMethod_4();
+        RandomMethod_5();
+        RandomMethod_6();
+        RandomMethod_7();
+        RandomMethod_8();
+        RandomMethod_9();
+        RandomMethod_10();
+        
+        // Call more random methods as needed
+    }
+
+    private void RandomMethod_1()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_2()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_3()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_4()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_5()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_6()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_7()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_8()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_9()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_10()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_11()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_12()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_13()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_14()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_15()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_16()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_17()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_18()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_19()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_20()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_21()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_22()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_23()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_24()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_25()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_26()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_27()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_28()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_29()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_30()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_31()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_32()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_33()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_34()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_35()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_36()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_37()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_38()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_39()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_40()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_41()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_42()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_43()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_44()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_45()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_46()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_47()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_48()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_49()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_50()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_51()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_52()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_53()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_54()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_55()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_56()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_57()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_58()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_59()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_60()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_61()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_62()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_63()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_64()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_65()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_66()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_67()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_68()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_69()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_70()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_71()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_72()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_73()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_74()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_75()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_76()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_77()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_78()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_79()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_80()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_81()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_82()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_83()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_84()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_85()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_86()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_87()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_88()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_89()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_90()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_91()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_92()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_93()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_94()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_95()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_96()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_97()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_98()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_99()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_100()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_101()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_102()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_103()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_104()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_105()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_106()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_107()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_108()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_109()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_110()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_111()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_112()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_113()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_114()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_115()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_116()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_117()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_118()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_119()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_120()
+    {
+        Debug.Log("Random Method 10");
+    }
+    
+    private void RandomMethod_121()
+    {
+        Debug.Log("Random Method 1");
+    }
+
+    private void RandomMethod_122()
+    {
+        Debug.Log("Random Method 2");
+    }
+
+    private void RandomMethod_123()
+    {
+        Debug.Log("Random Method 3");
+    }
+
+    private void RandomMethod_124()
+    {
+        Debug.Log("Random Method 4");
+    }
+
+    private void RandomMethod_125()
+    {
+        Debug.Log("Random Method 5");
+    }
+
+    private void RandomMethod_126()
+    {
+        Debug.Log("Random Method 6");
+    }
+
+    private void RandomMethod_127()
+    {
+        Debug.Log("Random Method 7");
+    }
+
+    private void RandomMethod_128()
+    {
+        Debug.Log("Random Method 8");
+    }
+
+    private void RandomMethod_129()
+    {
+        Debug.Log("Random Method 9");
+    }
+
+    private void RandomMethod_130()
+    {
+        Debug.Log("Random Method 10");
+    }
+}
