53fd9cd34faa917ff5e8f90b4efcfed8eac1c1d4
Terramen <mrdimon4eg@gmail.com>
2023-09-15 09:09:15 +0200

    Kek

---
+++ Assets/Stpircs/RandomScriptK.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScriptK : MonoBehaviour
+{
+    // Declare 100 string fields with random names
+    private string fieldA, fieldB, fieldC, fieldD, fieldE, fieldF, fieldG, fieldH, fieldI, fieldJ;
+    private string fieldK, fieldL, fieldM, fieldN, fieldO, fieldP, fieldQ, fieldR, fieldS, fieldT;
+    private string fieldU, fieldV, fieldW, fieldX, fieldY, fieldZ, fieldAA, fieldBB, fieldCC, fieldDD;
+    private string fieldEE, fieldFF, fieldGG, fieldHH, fieldII, fieldJJ, fieldKK, fieldLL, fieldMM, fieldNN;
+    private string fieldOO, fieldPP, fieldQQ, fieldRR, fieldSS, fieldTT, fieldUU, fieldVV, fieldWW, fieldXX;
+    private string fieldYY, fieldZZ, fieldAAA, fieldBBB, fieldCCC, fieldDDD, fieldEEE, fieldFFF, fieldGGG, fieldHHH;
+    private string fieldIII, fieldJJJ, fieldKKK, fieldLLL, fieldMMM, fieldNNN, fieldOOO, fieldPPP, fieldQQQ, fieldRRR;
+    private string fieldSSS, fieldTTT, fieldUUU, fieldVVV, fieldWWW, fieldXXX, fieldYYY, fieldZZZ, fieldAAAA, fieldBBBB;
+
+    private string fieldCCCC,
+        fieldDDDD,
+        fieldEEEE,
+        fieldFFFF,
+        fieldGGGG,
+        fieldHHHH,
+        fieldIIII,
+        fieldJJJJ,
+        fieldKKKK,
+        fieldLLLL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assign random words to the string fields
+        fieldA = "Apple";
+        fieldB = "Banana";
+        fieldC = "Cherry";
+        fieldD = "Date";
+        fieldE = "Elderberry";
+        fieldF = "Fig";
+        fieldG = "Grape";
+        fieldH = "Honeydew";
+        fieldI = "Iguana";
+        fieldJ = "Jackfruit";
+        fieldK = "Kiwi";
+        fieldL = "Lemon";
+        fieldM = "Mango";
+        fieldN = "Nectarine";
+        fieldO = "Orange";
+        fieldP = "Papaya";
+        fieldQ = "Quince";
+        fieldR = "Raspberry";
+        fieldS = "Strawberry";
+        fieldT = "Tangerine";
+        fieldU = "Ugli fruit";
+        fieldV = "Vanilla";
+        fieldW = "Watermelon";
+        fieldX = "Xigua";
+        fieldY = "Yuzu";
+        fieldZ = "Zucchini";
+        
+        
+        fieldAA = "HFewrewrwer";
+        fieldBB = "rwerwerwerw";
+        fieldCC = "hfghfghtryrthgdgf";
+        fieldDD = "eqweqwerqewrwf";
+        fieldEE = "fgdfghtryrtye";
+        fieldFF = "kyuiyuhghbn";
+        fieldGG = "rwerwfdssvxcv";
+        fieldHH = "htyuythfgbvcbcvbn";
+        fieldII = "rewerwerwerwerw";
+        fieldJJ = "bbcvbfghdgcvbcbc";
+        fieldKK = "rwrwerwrwetytrtyrty";
+        fieldLL = "qwqeqweqwdsfsdf";
+        fieldMM = "hrhfghfghtryrt";
+        fieldNN = "qeqweasdazfxc";
+        fieldOO = "p[op[opioghfghfg";
+        fieldPP = "gjhjkutyutyjghj";
+        fieldQQ = "ghfhftyrtyfghfgh";
+        fieldRR = "rtyrtdfgdfbdfbdg";
+        fieldSS = "tretertewrtwrwerw";
+        fieldTT = "vbcvbcbdfsfds";
+        fieldUU = "erwerwerwgdfgdfgdf fruit";
+        fieldVV = "utyuhfghjghjghjg";
+        fieldWW = "tertergdfgdfdfgdfg";
+        fieldXX = "tertertetyyuty";
+        fieldYY = "werewrwersdfsdf";
+        fieldZZ = "ytrtyfghfghbcvbcvbcvvb";
+        
+        fieldAAA = "GSrwerfdsfsdfs";
+        fieldBBB = "tryrtydfgdfgdfg";
+        fieldCCC = "nvbnrterdfg";
+        fieldDDD = "weqwewrdsfdsf";
+        fieldEEE = "oiouijhgnmbm";
+        fieldFFF = "eqweasdadf";
+        fieldGGG = "xfsrtetdfgdg";
+        fieldHHH = "nvbnrthyrhr";
+        fieldIII = "eqwweqeqw";
+        fieldJJJ = "fdsdfsdf";
+        fieldKKK = "bfbrgfyrt";
+        fieldLLL = "vbnrthrty";
+        fieldMMM = "yewrwrwer";
+        fieldNNN = "qweqweqe";
+        fieldOOO = "p[piopipjkljkl[opioghfghfg";
+        fieldPPP = "nvghrtyrtyfghf";
+        fieldQQQ = "yuyutyutyutyrtyr";
+        fieldRRR = "rewtertertdfgdfg";
+        fieldSSS = "fgdgreytyufghh";
+        fieldTTT = "rwrddfgdfgdfdfg";
+        fieldUUU = "rtertertgdfg fruit";
+        fieldVVV = "wrwerwersdfs";
+        fieldWWW = "fsdrwethffgh";
+        fieldXXX = "ghjtyutyutyu";
+        fieldYYY = "werwerytyuytuiyui";
+        fieldZZZ = "[puououiojkhkghjk";
+
+        // ... Assign more random words as needed
+    }
+    // Start is called before the first frame update
+}
